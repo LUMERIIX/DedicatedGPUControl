9615558698a9ac244a31f262c44d7e6ab2279fc7
LUMERIIX <lukas.jenni@comlab.ch>
2019-07-01 21:19:44 +0200

    add GPU usage command (nvwmi) (nvwmi cannot be installed)

--- En!En_DedicatedGPU.ps1
+++ En!En_DedicatedGPU.ps1
@@ -1,6 +1,17 @@
 ﻿$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 Import-Module -Name $ScriptDir\externals\DeviceManagement\Release\DeviceManagement.psd1 -Verbose
 Set-ExecutionPolicy RemoteSigned
+
+$computer = $env:COMPUTERNAME
+#$namespace = "ROOT\cimv2\NV"
+$gpuclass = "Gpu"
+#$GPU_Meters=Get-WmiObject -Class $gpuclass -ComputerName $computer -Namespace $namespace | Select-Object *
+$GPU_Meters=Get-WmiObject -Class $gpuclass -ComputerName $computer | Select-Object *
+
+$nvidia_model=$GPU_Meters.productName
+$gpu_usage=$GPU_Meters.percentGpuUsage
+Write-Host "$nvidia_model%"
+
 Write-Host "Dedicated GPU enable/disable PowerShell Script"
 $Selection = Read-Host -Prompt 'Select GPU state [enable/disable]'
 if($Selection)
