f25e3592300a379f295cd6ea5801bea66a1ad706
LUMERIIX <lukas.jenni@comlab.ch>
2019-07-01 15:31:00 +0200

    changed testdevice to matching GPU on Matebook X Pro (enable/disable teseted & works) +changed module path to relative submodule of repo

--- En!En_DedicatedGPU.ps1
+++ En!En_DedicatedGPU.ps1
@@ -1,6 +1,5 @@
-﻿#Import-Module -Name C:\data\Scripts\DeviceManagment\Release\DeviceManagement.psd1 -Verbose
-$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-Import-Module -Name $ScriptDir\DeviceManagment\Release\DeviceManagement.psd1 -Verbose
+﻿$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+Import-Module -Name $ScriptDir\externals\DeviceManagement\Release\DeviceManagement.psd1 -Verbose
 Set-ExecutionPolicy RemoteSigned
 Write-Host "Dedicated GPU enable/disable PowerShell Script"
 $Selection = Read-Host -Prompt 'Select GPU state [enable/disable]'
@@ -9,12 +8,12 @@ if($Selection)
     if($Selection -eq "enable")
     {
         Write-Host "Enable GPU"
-        Get-Device | where {$_.name -like "Xbox 360-Controller*"} | Enable-Device
+        Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Enable-Device
     }
     if($Selection -eq "disable")
     {
         Write-Host "Disable GPU"
-        Get-Device | where {$_.name -like "Xbox 360-Controller*"} | Disable-Device
+        Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Disable-Device
     }
     else
     {
