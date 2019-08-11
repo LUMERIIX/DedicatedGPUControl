22123dddc8a04c3dcb08c37dc59ca6f79ab0ec1a
LUMERIIX <lukas.jenni@comlab.ch>
2019-08-11 21:09:07 +0200

    Adjusted Path to DeviceManagment PS Script

--- src/En!En_DedicatedGPU.ps1
+++ src/En!En_DedicatedGPU.ps1
@@ -1,18 +1,26 @@
 ﻿$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-Import-Module -Name $ScriptDir\externals\DeviceManagement\Release\DeviceManagement.psd1 -Verbose
+$ScriptDirChanged = $ScriptDir.replace('src','externals')
+
+Import-Module -Name $ScriptDirchanged\DeviceManagement\Release\DeviceManagement.psd1 -Verbose
+
 $Enable = Get-ChildItem args[0]
 
-Write-Host $Enable
-
-if($Enable -eq "1")
+if($Selection)
 {
-    Write-Host "Enable GPU"
-    Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Enable-Device
-}
-if($Enable -eq "0")
-{
-    Write-Host "Disable GPU"
-    Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Disable-Device
+    if($Enable -eq "1")
+    {
+        Write-Host "Enable GPU"
+        Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Enable-Device
+    }
+    if($Enable -eq "0")
+    {
+        Write-Host "Disable GPU"
+        Get-Device | where {$_.name -like "NVIDIA GeForce*"} | Disable-Device
+    }
+    else
+    {
+        Write-Host "No Operation Selected!"
+    }
 }
 else
 {
